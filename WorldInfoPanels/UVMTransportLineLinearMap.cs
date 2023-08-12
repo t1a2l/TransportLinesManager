22bf6bf5c9519b51a8b6aa43edd2e1dc6fca04fc
Vincent Wong <vincentwwy168@gmail.com>
2023-08-12 21:16:12 +0800

    Rewrite code; make it clearer

--- WorldInfoPanels/UVMTransportLineLinearMap.cs
+++ WorldInfoPanels/UVMTransportLineLinearMap.cs
@@ -399,11 +399,35 @@ namespace TransportLinesManager.WorldInfoPanels
                         nextStationIdx += m_cachedStopOrder.Length;
                     }
                     Vector3 relativePosition = m_vehicleButtons.items[idx].relativePosition;
-                    relativePosition.y
-                        = (Singleton<VehicleManager>.instance.m_vehicles.m_buffer[vehicleId].m_flags & (Vehicle.Flags.Leaving)) != 0 ? (prevStationIdx * 0.75f) + (nextStationIdx * 0.25f)
-                        : (Singleton<VehicleManager>.instance.m_vehicles.m_buffer[vehicleId].m_flags & (Vehicle.Flags.Arriving)) != 0 ? (prevStationIdx * 0.25f) + (nextStationIdx * 0.75f)
-                        : (Singleton<VehicleManager>.instance.m_vehicles.m_buffer[vehicleId].m_flags & (Vehicle.Flags.Stopped)) != 0 ? prevStationIdx
-                        : (prevStationIdx * 0.5f) + (nextStationIdx * 0.5f);
+                    // determine basic relative position
+                    // 4 switch cases for each of 4 unscaled display states
+                    float weightingPrev, weightingNext;
+                    Vehicle.Flags vehicleFlags = Singleton<VehicleManager>.instance.m_vehicles.m_buffer[vehicleId].m_flags;
+                    if ((vehicleFlags & (Vehicle.Flags.Stopped)) != 0)
+                    {
+                        // vehicle stopped at a stop; current stop is "prev" because next stop is "next"
+                        weightingPrev = 1;
+                        weightingNext = 0;
+                    }
+                    else if ((vehicleFlags & (Vehicle.Flags.Leaving)) != 0)
+                    {
+                        // vehicle departing from a stop
+                        weightingPrev = 0.75f;
+                        weightingNext = 0.25f;
+                    }
+                    else if ((vehicleFlags & Vehicle.Flags.Arriving) != 0)
+                    {
+                        // vehicle arriving at the next stop
+                        weightingPrev = 0.25f;
+                        weightingNext = 0.75f;
+                    }
+                    else
+                    {
+                        // vehicle in progress to next stop
+                        weightingPrev = weightingNext = 0.5f;
+                    }
+                    // apply the basic relative position first
+                    relativePosition.y = prevStationIdx * weightingPrev + nextStationIdx * weightingNext;
                     relativePosition.y = ShiftVerticalPosition(relativePosition.y * m_kminStopDistance);
                     m_vehicleButtons.items[idx].relativePosition = relativePosition;
                 }
