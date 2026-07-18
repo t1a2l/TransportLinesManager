953179c549508a62be6fd3aa098730173f5e61f4
Tal <talrofeh@gmail.com>
2026-07-18 18:57:23 +0300

    override null fix

--- Overrides/BusAIOverrides.cs
+++ Overrides/BusAIOverrides.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using HarmonyLib;
 using TransportLinesManager.Cache.BuildingData;
 using TransportLinesManager.Data.Base.ConfigurationContainers.OutsideConnections;
@@ -14,11 +13,19 @@ namespace TransportLinesManager.Overrides
 
         public static bool GetColor(ushort vehicleID, ref Vehicle data, InfoManager.InfoMode infoMode, InfoManager.SubInfoMode subInfoMode, ref Color __result)
         {
+            var controller = TransportLinesManagerMod.Controller;
+            var buildingLines = controller?.BuildingLines;
+
+            if (buildingLines == null)
+            {
+                return true;
+            }
+
             if (data.m_leadingVehicle == 0 
                 && infoMode == InfoManager.InfoMode.None 
                 && data.m_transportLine == 0
                 && data.m_custom != 0
-                && TransportLinesManagerMod.Controller.BuildingLines[data.m_custom] is InnerBuildingLine cacheItem
+                && buildingLines[data.m_custom] is InnerBuildingLine cacheItem
                 && cacheItem.LineDataObject is OutsideConnectionLineInfo ocli)
             {
                 __result = ocli.LineColor;
--- Overrides/PassengerTrainAIOverrides.cs
+++ Overrides/PassengerTrainAIOverrides.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using HarmonyLib;
 using TransportLinesManager.Cache.BuildingData;
 using TransportLinesManager.Data.Base.ConfigurationContainers.OutsideConnections;
@@ -11,15 +10,21 @@ namespace TransportLinesManager.Overrides
     {
         [HarmonyPatch(typeof(PassengerTrainAI), "GetColor", [typeof(ushort), typeof(Vehicle), typeof(InfoManager.InfoMode), typeof(InfoManager.SubInfoMode)], [ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Normal, ArgumentType.Normal])]
         [HarmonyPrefix]
-#pragma warning disable IDE0060 // Remove unused parameter
         public static bool GetColor(ushort vehicleID, ref Vehicle data, InfoManager.InfoMode infoMode, InfoManager.SubInfoMode subInfoMode, ref Color __result)
-#pragma warning restore IDE0060 // Remove unused parameter
         {
+            var controller = TransportLinesManagerMod.Controller;
+            var buildingLines = controller?.BuildingLines;
+
+            if (buildingLines == null)
+            {
+                return true;
+            }
+
             if (data.m_leadingVehicle == 0 
                 && infoMode == InfoManager.InfoMode.None 
                 && data.m_transportLine == 0
                 && data.m_custom != 0
-                && TransportLinesManagerMod.Controller.BuildingLines[data.m_custom] is InnerBuildingLine cacheItem
+                && buildingLines[data.m_custom] is InnerBuildingLine cacheItem
                 && cacheItem.LineDataObject is OutsideConnectionLineInfo ocli)
             {
                 __result = ocli.LineColor;
