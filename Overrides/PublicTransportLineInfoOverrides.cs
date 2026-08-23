6283e9aa48d03c094b78ea64337f9ff1d81e3180
Tal <talrofeh@gmail.com>
2026-08-23 22:01:27 +0300

    cleanup

--- Overrides/PublicTransportLineInfoOverrides.cs
+++ Overrides/PublicTransportLineInfoOverrides.cs
@@ -7,9 +7,7 @@ namespace TransportLinesManager.Overrides
 	{
 		[HarmonyPatch(typeof(PublicTransportLineInfo), "RefreshData")]
         [HarmonyPrefix]
-#pragma warning disable IDE0060 // Remove unused parameter
         public static bool RefreshData(bool colors, bool visible)
-#pragma warning restore IDE0060 // Remove unused parameter
         {
             return false;
 		}
